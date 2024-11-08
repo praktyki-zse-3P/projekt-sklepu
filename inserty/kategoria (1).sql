4676811c7a16dbb510d610a6bda89d2753b443df
radekgryglicki <rudykuly@gmail.com>
2024-11-08 12:41:39 +0100

    Inserts

---
+++ inserty/kategoria (1).sql
@@ -0,0 +1,3 @@
+insert into kategoria (id_kategorii, plec) values (1, 'unisex');
+insert into kategoria (id_kategorii, plec) values (2, 'unisex');
+insert into kategoria (id_kategorii, plec) values (3, 'meskie');
